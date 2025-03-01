cdabee64240e75f22ce19bd25d27a3c5e0bcfdb2
stefanmagu <stefan_magu@mail.com>
2025-03-01 19:27:08 +0200

    added and tested user api

---
+++ backend/models/physiotherapistModel.js
@@ -0,0 +1,20 @@
+import mongoose from "mongoose";
+
+const physiotherapistSchema = new mongoose.Schema({
+    name: { type: String, required: true },
+    email: { type: String, required: true, unique: true },
+    password: { type: String, required: true },
+    image: { type: String, required: true },
+    speciality: { type: String, default: "Physiotherapist" },
+    degree: { type: String, required: true },
+    experience: { type: String, required: true },
+    about: { type: String, required: true },
+    available: { type: Boolean, default: true },
+    fees: { type: Number, required: true },
+    slots_booked: { type: Object, default: {} },
+    address: { type: Object, required: true },
+    date: { type: Number, required: true },
+}, { minimize: false })
+
+const physiotherapistModel = mongoose.models.physiotherapist || mongoose.model("physiotherapist", physiotherapistSchema);
+export default physiotherapistModel;
--- backend/server.js
+++ backend/server.js
@@ -3,13 +3,13 @@ import cors from 'cors'
 import 'dotenv/config'
 import connectDB from "./config/mongodb.js"
 import connectCloudinary from "./config/cloudinary.js"
-// import userRouter from "./routes/userRoute.js"
+import userRouter from "./routes/userRoute.js"
 // import doctorRouter from "./routes/doctorRoute.js"
 // import adminRouter from "./routes/adminRoute.js"
 
 // app config
 const app = express()
-const port = process.env.PORT || 4000
+const port = process.env.PORT || 4001
 connectDB()
 connectCloudinary()
 
@@ -18,7 +18,7 @@ app.use(express.json())
 app.use(cors())
 
 // api endpoints
-// app.use("/api/user", userRouter)
+app.use("/api/user", userRouter)
 // app.use("/api/admin", adminRouter)
 // app.use("/api/doctor", doctorRouter)
 
